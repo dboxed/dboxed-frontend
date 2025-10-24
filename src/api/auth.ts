818d571007382240af69278ff715f582fd1bad92
Alexander Block <ablock84@gmail.com>
2025-10-24 21:43:34 +0200

    Support passing OIDC scope

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -6,6 +6,7 @@ import { envVars } from "@/env.ts";
 export const userManager = new UserManager({
   authority: envVars.VITE_OIDC_ISSUER_URL,
   client_id: envVars.VITE_OIDC_CLIENT_ID,
+  scope: envVars.VITE_OIDC_SCOPE,
   redirect_uri: `${window.location.origin}${window.location.pathname}`,
   post_logout_redirect_uri: window.location.origin,
   userStore: new WebStorageStateStore({ store: window.sessionStorage }),
